a10553e7f779be0bf94a8b03907be21a6342249c
Limheonjun <jumong40@naver.com>
2020-01-21 14:25:15 +0900

    Java5: Static Import

---
+++ Java5/src/StaticImport/Main.java
@@ -0,0 +1,17 @@
+package StaticImport;
+
+import static java.lang.Math.PI;
+import static java.lang.Math.abs;
+import static java.lang.System.out;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i = abs(-6);
+		out.println(i);
+		out.println(PI);
+		
+
+	}
+
+}
---
+++ Java5/src/StaticImport/README.md
@@ -0,0 +1,4 @@
+# Static Import
+
+## Decription
+- static멤버 호출시 클래스명 생략 가능
