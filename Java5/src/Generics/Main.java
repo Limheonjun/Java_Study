788014ece0c603e9826a5c1da2ae7e907a74b5a7
Limheonjun <jumong40@naver.com>
2020-01-21 14:36:08 +0900

    Java5: Generics

---
+++ Java5/src/Generics/Main.java
@@ -0,0 +1,16 @@
+package Generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<Integer> l = new ArrayList();
+		l.add(1);
+		l.add("2"); // Error
+		
+
+	}
+
+}
