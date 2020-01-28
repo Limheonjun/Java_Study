afe8ce0f622c78f040a445f863d1f207bfa4de76
Limheonjun <jumong40@naver.com>
2020-01-28 10:23:07 +0900

    Java9: Private Interface Method

--- Java9/src/PrivateInterfaceMethod/Main.java
+++ Java9/src/PrivateInterfaceMethod/Main.java
@@ -4,12 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// Use Private Method
-		UsePrivateMethod use = new UsePrivateMethod();
+		UsePrivateMethod use = new UsePrivateMethod() {};
 		use.method1();
 		use.method2();
 		
 		// Not Use Private Method
-		NotUsePrivateMethod nuse = new NotUsePrivateMethod();
+		NotUsePrivateMethod nuse = new NotUsePrivateMethod() {};
 		use.method1();
 		use.method2();
 		
